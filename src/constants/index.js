5305dd020fd2aa5041ba18b448e1689faaf9095d
Pravesh <ypravesh0007@gmail.com>
2024-09-05 00:34:57 +0530

    last

--- src/constants/index.js
+++ src/constants/index.js
@@ -132,7 +132,7 @@ export const projects = [
         theme: 'btn-back-pink',
         name: 'Portfolio',
         description: 'Built a complete 3d and animated portfolio .',
-        link: 'https://github.com/adrianhajdin/social_media_app',
+        link: 'https://courageous-swan-99685b.netlify.app/',
     },
     
 ];
