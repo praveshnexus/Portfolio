770f8446b2e311e4a7658a49eaa89fafe2f9816b
Pravesh <ypravesh0007@gmail.com>
2024-09-04 23:55:19 +0530

    minor change

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 export {Sky} from "./Sky";
 export {Bird} from "./Bird";
 export {Plane} from "./Plane";
-export {Island} from "./island"; // Corrected to match the file name
+export {Island} from "./Island"; // Corrected to match the file name
 export {Fox} from "./Fox";
